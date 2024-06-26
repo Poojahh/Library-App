ac6c6b3300e4e8a89835783f35131842a8f1ddf5
Poojahh <poojakrishnan1104@gmail.com>
2024-06-26 13:58:14 +0530

    Library App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <HomePage/>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/about" element={<AboutPage/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
