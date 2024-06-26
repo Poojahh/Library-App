910261050e22ef655de6c5547ff847224895e21b
Poojahh <poojakrishnan1104@gmail.com>
2024-06-26 13:13:57 +0530

    Library App

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <center><h1>Welcome All</h1></center>
+                        <center><h2>Library App is here!!</h2></center>
+                        <center><img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpfIiae5jo8yUiTJnNcYBqKaIzAjX4dq2wGQ&s"></img></center>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h3>Description</h3>
+                        <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias ipsum expedita laborum tempore voluptatem maiores! Qui fugit dolorum magni, hic deleniti consequatur quis laudantium, exercitationem nulla, quas ipsa adipisci similique!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque porro perferendis provident repellat placeat a, quia quos quo odit eligendi, adipisci eaque quod quam, debitis nisi. Omnis voluptatum autem cupiditate.</h5>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h3>Main Features</h3>
+                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non facilis reiciendis voluptatibus possimus molestiae, sunt quaerat. Dolorem quia id laborum, quasi saepe quibusdam? Dignissimos aliquid officia tempora molestiae delectus laudantium? ipsum dolor, sit amet consectetur adipisicing elit. Commodi nesciunt qui explicabo numquam? Odio, incidunt dolore! Consequuntur, totam accusantium assumenda officiis quia magni molestiae autem reprehenderit recusandae. Minima, cum minus!</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
