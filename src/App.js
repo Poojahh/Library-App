7ffb41ea1c2b7840de7ea67000ad4a2ccd393f3a
Poojahh <poojakrishnan1104@gmail.com>
2024-06-26 16:23:27 +0530

    Library App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ServicesPage from './components/ServicesPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Route path="/" element={<HomePage/>}/>
     <Route path="/about" element={<AboutPage/>}/>
     <Route path="/service" element={<ServicesPage/>}/>
+    <Route path="/contact" element={<ContactPage/>}/>
    </Routes>
    </BrowserRouter>
   );
---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Subject</label>
+                        <select name="" id="" className="form-control">
+                            <option value="select">select</option>
+                            <option value="Science">Science</option>
+                            <option value="History">History</option>
+                            <option value="Maths">Maths</option>
+                            <option value="English">English</option>
+                            <option value="Others">Others</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Phone no</label>
+                        <input type="text" className="form-control" /> 
+                     </div>  
+                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                       <label htmlFor="" className="form-label">Message</label>
+                       <textarea name="" id="" className="form-control"></textarea>
+                    </div>
+                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-info">Contact</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+  )
+}
+
+export default ContactPage
